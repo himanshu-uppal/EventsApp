4d6fc7203caac71646667afb1e1be32320b01ad9
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-11 11:51:37 +0530

    making child components and passign data in child component using @Input

---
+++ src/app/events-list/events-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+  event ={
+    id:1,
+    name:'Fest',
+    price:599
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
