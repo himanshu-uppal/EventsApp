cea2911a1f210e5b25136815e99c9e4cde00c543
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-15 10:41:36 +0530

    used a simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule, Routes } from '@angular/router';
+import { EventsListComponent } from './events-list/events-list.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
+
+const routes: Routes = [
+  {path:'events',component:EventsListComponent},
+  {path:'events/:id',component:EventDetailsComponent},
+  {path:'',redirectTo:'events',pathMatch:'full'}
+]
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }
