31c97f300b958fc47bc2b8d1d476278b3d2250f2
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-15 16:05:55 +0530

    lazy loading feature module and different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {path:'events',component:EventsListComponent,resolve:{events:EventListResolver}},
   {path:'events/:id',component:EventDetailsComponent,canActivate:[EventRouteActivator]},
   {path:'404',component:E404Component},
-  {path:'',redirectTo:'events',pathMatch:'full'}
+  {path:'',redirectTo:'events',pathMatch:'full'},
+  {path:'user',loadChildren:'./user/user.module#UserModule'}
 ]
 
 @NgModule({
---
+++ src/app/user/profile.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core'
+
+@Component({
+    template:`<h1>Profile</h1>
+    <div>[Edit profile]</div>
+    <div >
+    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-default">Cancel</button>
+    </div>
+    `
+
+})
+export class ProfileComponent{
+
+}
