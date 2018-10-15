5c5f3a987d2ce05c756594c6dc39625a734cd2b3
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-15 10:35:38 +0530

    created routes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events-list/events-list.component';
 import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 import {EventService} from './shared/services/event.service'
-import {ToastrService} from './shared/services/toastr.service'
+import {ToastrService} from './shared/services/toastr.service';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    EventDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [EventService,ToastrService],
   bootstrap: [EventsAppComponent]
